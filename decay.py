2466f3a0861e0fbb77218962d7a1be54ecd7b7e3
David Caratelli <dcaratelli@ucsb.edu>
2022-03-21 23:00:58 -0700

    adding basic scripts --DC

---
+++ decay.py
@@ -0,0 +1,103 @@
+import numpy as np
+from math import pi, sqrt, log10, log
+import numpy as np
+
+_aEM = 1./137. #
+_Gf  = 1.166 * 1e-5 # GeV-2
+_m_pi0 = 0.134977 # GeV
+_m_pi = 0.139570 # GeV
+_f_pi = 0.13041 # GeV [??? wikipedia mentions convention differing by sqrt(2)...]
+
+_m_elec = 0.000511 # GeV
+_m_muon = 0.105658 # GeV
+
+_Vud2 = 0.97427**2 # |V_ud|^2
+
+_sW = 0.22290 # square of sine of the Weinberg angle
+_C1 = (1/4.) * (1 - 4 * _sW + 8 * _sW**2)
+_C2 = (1/2.) * _sW * (2 * _sW - 1)
+_C3 = (1/4.) * (1 + 4 * _sW + 8 * _sW**2)
+_C4 = (1/2.) * _sW * (2 * _sW + 1)
+
+def _DIRAC(alpha,beta):
+    if (alpha==beta): return 1
+    return 0
+
+def _LEPTON_MASS(alpha):
+    if (alpha==1): return _m_elec
+    if (alpha==2): return _m_muon
+    if (alpha==3): return _m_muon
+    return 0
+
+def _L(yl2): # ??? is this log10
+    N = 1-4*yl2
+    if (N < 0): return -1
+    N = sqrt(N)
+    B = ( 1 - 3*yl2 - (1-yl2) * N ) / ( yl2 * (1 + N ) )
+    #print ("N = %g"%N)
+    #print ("B = %g"%B)
+    if (B <= 0): return -1
+    return log( B )
+
+
+# channel decaying to \nu + \nu + \nu [A.1]
+
+def _Gamma_nununu(HNL_MASS,U):
+    return _Gf**2 * HNL_MASS**5 * U**2 / (192 * pi**3)
+
+# channel decaying to \nu_{\alpha} + \gamma [A.2]
+def _Gamma_nugamma(HNL_MASS,U):
+    return 9 * _aEM * _Gf**2 * HNL_MASS**5 * U**2 / (512 * pi**4)
+
+# channel decaying to \pi^0 + \nu_{\alpha} [A.3]
+def _Gamma_nupi0(HNL_MASS,U):
+    if (HNL_MASS < _m_pi0): return 0
+    return ( (_Gf**2 * _f_pi**2 * HNL_MASS**3 * U**2) / (32 * pi) ) * ( 1 - (_m_pi0/HNL_MASS)**2 )**2
+
+# channel decaying to \pi^+ and lepton [A.4]
+def _Gamma_pilepton(HNL_MASS,U,alpha):
+    ML = _LEPTON_MASS(alpha)
+    if (HNL_MASS < (_m_pi + ML)): return 0
+    A = (1 - ( ( (_m_pi - ML) / HNL_MASS )**2 ) ) * (1 - ( (_m_pi + ML) / HNL_MASS )**2 )
+    if (A <= 0): return 0
+    return ( _Gf**2 * _f_pi**2 * HNL_MASS**3 * _Vud2 * U**2 / (16 * pi) ) * ( ( 1 - (_LEPTON_MASS(alpha)/HNL_MASS)**2 )**2 - (_m_pi/HNL_MASS)**2 * (1 + (_LEPTON_MASS(alpha) / HNL_MASS)**2 ) )**2 * sqrt( A )
+
+# channel decaying to l^{-}_{\alpha} + l^{+}_{\beta} + \nu_{\beta} [A.5]
+# ??? is log in this expression base 10?
+def _Gamma_llnu1(HNL_MASS,U,alpha,beta):
+    if (alpha == beta): return 0
+    if (HNL_MASS < (_LEPTON_MASS(alpha) + _LEPTON_MASS(beta)) ): return 0
+    #xl = max(_LEPTON_MASS(beta),_LEPTON_MASS(alpha)) / HNL_MASS
+    xl = _m_elec / HNL_MASS
+    return ( _Gf**2 * HNL_MASS**5 * U**2 / (192 * pi**3) ) * (1 - 8 * xl**2 + 8 * xl**6 - xl**8 - 12 * xl**4 * log(xl**4) )
+
+# channel decaying to l^{-}_{\beta} + l^{+}_{\beta} + \nu_{\alpha} [A.6]
+def _Gamma_llnu2(HNL_MASS,U,alpha,beta):
+    if (HNL_MASS < 2 * _LEPTON_MASS(beta)): return 0
+    yl2 = (_LEPTON_MASS(beta) / HNL_MASS)**2
+    #print ("yl2 = %g"%yl2)
+    A = 1-4*yl2
+    #print ("A = %g"%A)
+    if (A <= 0): return 0
+    A = sqrt(A)
+    delta = _DIRAC(alpha,beta)
+    L = _L(yl2)
+    #print ("L = %g"%L)
+    if (L == -1): return 0
+    TERM1 = ( _Gf**2 * HNL_MASS**5 * U**2 / (192 * pi**3) )
+    TERM2 = (_C1 * (1 - delta ) + _C3 * delta )
+    TERM3 = ( ( 1 - 14*yl2 - 2*yl2**2 - 12*yl2**3 ) * A + 12*yl2**2 * (yl2**2-1) * L )
+    TERM4 = 4 * (_C2 * (1-delta) + _C4 * delta) * (yl2 * (2+10*yl2-12*yl2**2) * A + 6*yl2**2*(1-2*yl2+2*yl2**2) * L )
+    #print ("TERM1 : %g"%TERM1)
+    #print ("TERM2 : %g"%TERM2)
+    #print ("TERM3 : %g"%TERM3)
+    #print ("TERM4 : %g"%TERM4)
+    return TERM1 * ( TERM2 * TERM3 + TERM4 )
+
+
+_Gamma_nununu_v   = np.vectorize(_Gamma_nununu)
+_Gamma_nugamma_v  = np.vectorize(_Gamma_nugamma)
+_Gamma_nupi0_v    = np.vectorize(_Gamma_nupi0)
+_Gamma_pilepton_v = np.vectorize(_Gamma_pilepton)
+_Gamma_llnu1_v    = np.vectorize(_Gamma_llnu1)
+_Gamma_llnu2_v    = np.vectorize(_Gamma_llnu2)
---
+++ production.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+# load BNB neutrino flux [from MicroBooNE paper XXX]
+
+def MicroBooNE_FLUX():
+    flux_data = np.loadtxt('MicroBooNE_Flux_Pions_Kaons.csv',skiprows=2,delimiter=',')
+
+    # flux in units of neutrinos / POT / GeV / cm2
+    kaon_flux = []
+    pion_flux = []
+    for flux_row in flux_data:
+        kaon_f = flux_row[1]
+        pion_f = flux_row[3]
+        kaon_flux.append(kaon_f)
+        pion_flux.append(pion_f)
+
+    MAXFLUXPION = np.max(pion_flux)
+    MAXFLUXKAON = np.max(kaon_flux)
+    BINWIDTH = 0.05 # GeV
+
+# sample from pion/kaon BNB flux at MicroBooNE
+# input MASS : HNL mass
+# input MESON: pion (211) or kaon (411)
+# input N    : number of HNLs to simulate
+# return Kinetic Energy in MeV
+def BNB_HNL_ENERGY(MASS,MESON,N):
+    simulated_v = []
+
+    # set constants appropriately based on whether simulating a pion or a kaon
+
+    # set maximum energy to sample
+    # set which flux to use 
+    ENERGY_MAX = 0. # GeV
+    MAXFLUX = 0
+    flux_v = []
+    if (MESON == 211):
+        ENERGY_MAX = 2.95 # GeV
+        flux_v = _PION_FLUX
+        MAXFLUX = _MAXFLUXPION
+    elif (MESON == 411):
+        ENERGY_MAX = 4.95 # GeV
+        flux_v = _KAON_FLUX
+        MAXFLUX = _MAXFLUXKAON
+    else:
+        return simulated_v
+
+    while len(simulated_v) < N:
+        # simulate an energy from 0 to ENERGY_MAX #GeV
+        energy = np.random.uniform(MASS/1e3,ENERGY_MAX)
+        # find the bin it falls in [50 MeV bins]
+        energybin = int(energy/_BINWIDTH)
+        # probability of seeing an event in this energy bin
+        p = np.random.uniform(0,_MAXFLUX)
+        #print (' p is ',p)
+        # is this value smaller than flux prediction at this energy? 
+        # if so, continue to simulate event
+        if (p < flux_v[energybin]):
+            simulated_v.append(energy*1e3-MASS)
+    return np.array(simulated_v)
+
+# from article 1912.07622
+
+# strength based on mixing [7.2]
+def MIXING(U):
+    return ((U**2)/(1-U**2))
+
+# input are the masses of the meson, HNL, and lepton respectively [7.2]
+def RHO(MESON,HNL,LEPTON):
+    x = (LEPTON/MESON)**2
+    y = (HNL/MESON)**2
+    return ((x+y-(x-y)**2)) * np.sqrt(1+x**2+y**2-2*(x+y+x*y)) / (x*(1-x)**2)
